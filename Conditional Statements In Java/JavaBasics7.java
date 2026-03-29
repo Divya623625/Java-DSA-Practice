8654aeeab0b084ea5e02d1b37d207f66fbd89a01
Divya P <divyadivyap9817@gmail.com>
2026-03-29 23:27:00 +0530

    Added program demonstrating ternary operator

---
+++ Conditional Statements In Java/JavaBasics7.java
@@ -0,0 +1,7 @@
+public class JavaBasics7 {
+    public static void main(String args[]){
+        int number=4;
+        String type=((number%2)==0)?"EVEN":"ODD";
+        System.out.println(type);
+    }
+}
