8c88ca31532dec27888f12d313456992e2148b67
Divya P <divyadivyap9817@gmail.com>
2026-03-29 15:44:09 +0530

    Added Java Program on type promotion and valid identifiers

--- Variables and Datatypes/JavaBasics18.java
+++ Variables and Datatypes/JavaBasics18.java
@@ -18,7 +18,8 @@ public class JavaBasics18{
 Question 5: (Advanced) Will the following statement give any error in Java
 int $ = 24;
 No, the statement will not give any error.
-NamesofvariablesarecalledidentifiersinJava. Identifierrulesays, identifierscanstartwith
-any alphabet or underscore (“_”) or dollar (“$”).
+Names of variables are called identifiers in Java. 
+Identifier rule says:
+identifiers can start with any alphabet or underscore (“_”) or dollar (“$”).
 According to the rule the given variable name is a valid identifier
  */
