d7fdfecc28df5d140b60fef381d4fafc78a07d8c
Divya P <divyadivyap9817@gmail.com>
2026-03-29 15:43:24 +0530

    Added Java Program on type promotion and valid identifiers

---
+++ Variables and Datatypes/JavaBasics18.java
@@ -0,0 +1,24 @@
+// Question 4: What will be the type of result in the following Java code?
+// (Hint : Look at the largest data type among these)
+public class JavaBasics18{
+    public static void main(String args[]){
+        byte b=4;
+        char ch='a';
+        short s=512;
+        int i =1000;
+        float f=3.14f;
+        double d=99.9954;
+        double result=(f*d)+(i%ch-(d*s)+b);
+        System.out.println(result);
+    }
+}
+// In the mentioned code, the result variable will be of double type because of type conversion
+
+/*
+Question 5: (Advanced) Will the following statement give any error in Java
+int $ = 24;
+No, the statement will not give any error.
+NamesofvariablesarecalledidentifiersinJava. Identifierrulesays, identifierscanstartwith
+any alphabet or underscore (“_”) or dollar (“$”).
+According to the rule the given variable name is a valid identifier
+ */
