f84196055126d89868a09b93e2556bbf475dcb85
Divya P <divyadivyap9817@gmail.com>
2026-03-29 15:06:37 +0530

    Added Java program for sum of 2 numbers with comments

---
+++ Variables and Datatypes/JavaBasics5.java
@@ -0,0 +1,18 @@
+public class JavaBasics5 {
+    public static void main(String args[]){
+        // Code to calculate sum => Single line comment
+        int a = 10;
+        int b = 5;
+        int sum=a+b;
+        System.out.println(sum);
+    }    
+}
+/*
+Multi line comment : 
+a = 10 → value 10 is stored in memory, and a points to that location
+b = 5 → value 5 is stored in another memory location, b points there
+sum = a + b → CPU adds 10 + 5 = 15 and stores 15 in a new memory location for sum
+So 👉 values are stored separately, and variable names just refer to their memory locations. 
+*/
+
+ 
