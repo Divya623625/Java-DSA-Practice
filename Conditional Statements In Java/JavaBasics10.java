013de455f9888b4aa455b3e96e494a7588b34527
Divya P <divyadivyap9817@gmail.com>
2026-03-29 23:31:02 +0530

    Added Java program demonstrating calculator

---
+++ Conditional Statements In Java/JavaBasics10.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class JavaBasics10 {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char operator=sc.next().charAt(0);
+        switch(operator){
+            case '+' -> System.out.println(a+b);
+            case '-' -> System.out.println(a-b);
+            case '*' -> System.out.println(a*b);
+            case '/' -> System.out.println(a/b);
+            case '%' -> System.out.println(a%b);
+            default -> System.out.println("Wrong Operator");
+        }
+        sc.close();
+    }
+}
