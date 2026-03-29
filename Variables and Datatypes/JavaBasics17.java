9e702adef010fc28d4d7c8c94c3661b0ec32352c
Divya P <divyadivyap9817@gmail.com>
2026-03-29 15:39:19 +0530

    Added Java Program demonstrating the bill of items

---
+++ Variables and Datatypes/JavaBasics17.java
@@ -0,0 +1,25 @@
+/*
+Question 3: Enter cost of 3 items from the user (using float data type)- a pencil, a pen and
+an eraser. You have to output the total cost of the items back to the user as their bill.
+(Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem)
+*/
+import java.util.*;
+public class JavaBasics17 {  
+// Bill of Items
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float pencil = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float total = pencil + pen + eraser;
+        System.out.println("Bill is : " + total);
+        //Add on - with 18% tax
+        float newTotal = total + (0.18f * total);
+        System.out.println("Bill with 18% tax : " + newTotal);
+        sc.close();
+    }
+}
+// Tax = Income × Tax Rate 
+// For example, if you earn ₹50,000 and tax rate is 10%, then tax = 50,000 × 10% = ₹5,000.
+
