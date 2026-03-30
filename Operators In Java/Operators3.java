78fca9c69b602f96f4926d4d1683bcd6376f0586
Divya P <divyadivyap9817@gmail.com>
2026-03-30 11:48:51 +0530

    Added Java Program demonstrating Relational Operators

---
+++ Operators In Java/Operators3.java
@@ -0,0 +1,17 @@
+public class Operators3{
+    public static void main(String args[]){
+        int A = 10;
+        int B = 10;
+        int C = 10;
+        int D = 5;
+        
+        System.out.println(A==B);
+        System.out.println(A!=B);
+        System.out.println(C>D);
+        System.out.println(D>C);        
+        System.out.println(C<D);  
+        System.out.println(D<C);
+        System.out.println(A>=B);
+        System.out.println(A<=B);
+    }    
+}
