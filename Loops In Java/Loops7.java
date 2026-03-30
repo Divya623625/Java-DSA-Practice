c9a3c966ac4d9ec87a7b18f6b1deeda06db4089e
Divya P <divyadivyap9817@gmail.com>
2026-03-30 12:57:43 +0530

    Added Printing square pattern program

---
+++ Loops In Java/Loops7.java
@@ -0,0 +1,15 @@
+public class Loops7 {
+    public static void main(String args[]){
+        for(int line=1;line<=4;line++){
+            System.out.println("* * * *");
+        }
+    }
+}
+/*
+Using while loop
+int line=1;
+while(line<=4){
+    System.out.println("* * * *");
+    line++;
+}
+*/
