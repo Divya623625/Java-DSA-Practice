0deb3c2e45d97d5964daf2bdaedfd2636189e4da
Divya P <divyadivyap9817@gmail.com>
2026-03-30 11:32:38 +0530

    Added Java Program demonstrating if-else

---
+++ Conditional Statements In Java/CS1.java
@@ -0,0 +1,31 @@
+public class CS1{
+    public static void main(String args[]){
+        
+        int age=22;
+        if(age>18){
+            System.out.println("Adult:Drive,Vote");
+        }
+        else{
+            System.out.println("Not adult");
+        }
+        
+        int age1=16;
+        if(age1>18){
+            System.out.println("Adult:Drive,Vote");
+        }
+        else{
+            System.out.println("Not adult");
+        }
+        
+        if(age>13 && age<18){
+            System.out.println("Teenager");
+        }
+        if(age>=18){
+            System.out.println("Adult:Drive,Vote");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+    
+}
