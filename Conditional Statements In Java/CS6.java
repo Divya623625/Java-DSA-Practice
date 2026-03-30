d0cdc63c7607ddf25a476e19c3360fb47fde5d6d
Divya P <divyadivyap9817@gmail.com>
2026-03-30 11:36:30 +0530

    Added Largest of 3 numbers program

---
+++ Conditional Statements In Java/CS6.java
@@ -0,0 +1,14 @@
+public class CS6 {
+    public static void main(String args[]){
+        int A=1,B=7,C=6;
+        if((A>=B) && (A>=C)){
+            System.out.println(A);
+        }
+        else if(B>=C){
+            System.out.println(B);
+        }
+        else{
+            System.out.println(C);
+        }
+    }   
+}
