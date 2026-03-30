1db0fd20d5844f9040e74351e419b1f9f799e634
Divya P <divyadivyap9817@gmail.com>
2026-03-30 11:33:54 +0530

    Added Java Program demonstrating Largest of 2 numbers

---
+++ Conditional Statements In Java/CS2.java
@@ -0,0 +1,13 @@
+public class CS2 {
+    public static void main(String args[]){
+        int A=1;
+        int B=5;
+
+        if (A>=B){
+            System.out.println("A is largest of 2"); System.out.println(A);
+        }
+        else{
+            System.out.println("B is largest of 2"); System.out.println(B);
+        }
+    }  
+}
