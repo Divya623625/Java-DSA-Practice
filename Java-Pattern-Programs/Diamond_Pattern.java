3f3eeb8ea2aefce05844a7a40cf11b32f016c105
Divya P <divyadivyap9817@gmail.com>
2026-04-02 19:14:23 +0530

    Added Diamond Pattern in Java Program

---
+++ Java-Pattern-Programs/Diamond_Pattern.java
@@ -0,0 +1,27 @@
+public class Diamond_Pattern {
+    public static void main(String[] args) {
+        int n=4;
+        // half height of the diamond ( no of rows in top half )
+        // upper half - print spaces , print stars
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        // lowwer half
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            // print stars
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
