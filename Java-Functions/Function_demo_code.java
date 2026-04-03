a57a8b35829f73b1c5f5f0343c1560c0cb264230
Divya P <divyadivyap9817@gmail.com>
2026-04-03 13:30:08 +0530

    Added Function demo program in java

---
+++ Java-Functions/Function_demo_code.java
@@ -0,0 +1,11 @@
+public class Function_demo_code{
+    public static int printHelloWorld(){
+        System.out.println("Hellow World!");
+        System.out.println("Hellow World!");
+        System.out.println("Hellow World!");
+        return 3;
+    }
+    public static void main(String[] args) {
+        printHelloWorld(); // This is function call
+    }    
+}
