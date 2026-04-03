aa98a53643cd82215caab79b75f002488bac1264
Divya P <divyadivyap9817@gmail.com>
2026-04-03 13:25:16 +0530

    Added Binomial Coefficient program in java

---
+++ Java-Functions/Binomial_Coeff.java
@@ -0,0 +1,20 @@
+public class Binomial_Coeff {
+        public static int factorial(int n){
+            int f=1;
+            for(int i=1;i<=n;i++){
+                f = f * i;
+            }
+            return f;
+        }
+        public static int binCoeff(int n,int r){
+            int fact_n=factorial(n);
+            int fact_r=factorial(r);
+            int fact_nmr=factorial(n-r);
+
+            int bin_coeff=fact_n/(fact_r*fact_nmr);
+            return bin_coeff; 
+        }
+        public static void main(String[] args) {
+            System.out.println(binCoeff(5,2));
+        }
+    }
