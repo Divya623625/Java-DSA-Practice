365b30f353d08d985025a0ed696852ec5dea84dc
Divya P <divyadivyap9817@gmail.com>
2026-04-03 18:13:04 +0530

    Added Sum of digits program in Java

---
+++ Java-Functions/Sum_Of_Digits.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Sum_Of_Digits {
+    public static int sum(int n){
+        int sumOfdigits = 0;
+        while(n > 0){
+            int lastDigit = n % 10;
+            sumOfdigits += lastDigit;
+            n /= 10;
+        }
+        return sumOfdigits;
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input an Integer");
+        int digits = sc.nextInt();
+        System.out.println("The Sum is" + sum(digits));
+        sc.close();
+    }   
+}
+    
