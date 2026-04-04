5ee31d6a4d414af8e2c66244d45bd64786ec43bb
Divya P <divyadivyap9817@gmail.com>
2026-04-04 18:35:03 +0530

    Implemented Floyd's Triangle Pattern Using Functions

---
+++ Java-Patterns-Advanced/Floyds_Triangle.java
@@ -0,0 +1,21 @@
+public class Floyds_Triangle {
+    public static void Floys_triangle(int n){
+        // outer
+        int counter=1;
+        for(int i=1;i<=n;i++){
+            // inner - how many times will counter be printed
+            for(int j=1;j<=i;j++){
+                System.out.print(counter +" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Floys_triangle(10);
+    }
+}
+
+    
+        
+
