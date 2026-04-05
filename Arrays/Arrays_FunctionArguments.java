87252a05b302a80e2f47e0078fb455212a48bde2
Divya P <divyadivyap9817@gmail.com>
2026-04-05 19:05:46 +0530

    Added array update using function arguments

---
+++ Arrays/Arrays_FunctionArguments.java
@@ -0,0 +1,20 @@
+public class Arrays_FunctionArguments{
+    public static void update(int marks[], int nonChangeable){
+        nonChangeable = 10;
+        for(int i = 0; i < marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = { 25, 55, 100};
+        int nonChangeable = 5;
+        update(marks, nonChangeable);
+        System.err.println(nonChangeable);
+
+        // print our marks
+        for(int i = 0; i < marks.length; i++){
+            System.out.print(marks[i] + "  ");
+        }
+        System.out.println();
+    }
+}
