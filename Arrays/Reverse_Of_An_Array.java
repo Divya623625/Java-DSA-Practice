2bd70d45993f23162f91853b3be73e719146a6fc
Divya P <divyadivyap9817@gmail.com>
2026-04-05 21:32:40 +0530

    Implemented Reverse Of An Array Program

---
+++ Arrays/Reverse_Of_An_Array.java
@@ -0,0 +1,24 @@
+public class Reverse_Of_An_Array{
+    public static void reversearray(int numbers[]){
+        int first = 0; int last = numbers.length-1;
+        while( first < last ){
+            // swap
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+
+            first++;
+            last--; 
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[]={ 10,20,30,40,50 };
+        reversearray(numbers);
+
+        // print
+        for(int i=0; i< numbers.length; i++){
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
+    }
+}
