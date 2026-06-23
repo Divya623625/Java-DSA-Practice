76f46a9641b9e50692526d99f53d871e50e1468b
Divya P <divyadivyap9817@gmail.com>
2026-06-23 14:25:12 +0530

    Added Java program for area of circle(input from user)

--- Variables and Datatypes/JavaBasics9.java
+++ Variables and Datatypes/JavaBasics9.java
@@ -3,9 +3,9 @@ public class JavaBasics9 {
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        Float rad=sc.nextFloat();
+        float rad=sc.nextFloat();
         System.out.println("Radius is" + rad);
-        Float area=3.14f * rad * rad;
+        float area=3.14f * rad * rad;
         System.out.println("Area of a circle" + area);
         sc.close();
     }    
