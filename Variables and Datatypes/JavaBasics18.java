4c0acbbbbfba488c4e13e53d3db72de4c7705c35
Divya P <divyadivyap9817@gmail.com>
2026-06-27 23:46:05 +0530

    Updated java program on type promotion and valid identifiers

--- Variables and Datatypes/JavaBasics18.java
+++ Variables and Datatypes/JavaBasics18.java
@@ -3,12 +3,12 @@
 public class JavaBasics18{
     public static void main(String args[]){
         byte b=4;
-        char ch='a';
+        char c='a';
         short s=512;
         int i =1000;
         float f=3.14f;
         double d=99.9954;
-        double result=(f*d)+(i%ch-(d*s)+b);
+        double result=(f*b)+(i%c)-(d*s);
         System.out.println(result);
     }
 }
